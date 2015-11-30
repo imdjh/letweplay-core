23bdbf424bc77b80f1b58554d04595c8441e5b81
Jiahao Dai <20123225@student.shiep.edu.cn>
2015-11-30 13:39:55 +0800

    Fix WEPLAY_ROM MAGIC.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 export WEPLAY_SAVE_INTERVAL=${SAVE_DELAY:-120000}
-
+export WEPLAY_ROM=${WEPLAY_ROM:-"/usr/src/game-data"}
 
 bad_selection() {
     echo 'Bad GAME selection, please try another one!'
@@ -13,12 +13,11 @@ export DN_SERVER=${DN_SERVER:-http://imdjh-dn.daoapp.io}
 
 if [ ! -z "${GAME}" ];then
     echo "Fetching game..."
-    curl -fsSL ${DN_SERVER}/${GAME} -o /usr/src/game-data || bad_selection
-    file /usr/src/game-data > /var/tmp/game-data.filetype
+    curl -fsSL ${DN_SERVER}/${GAME} -o ${WEPLAY_ROM} || bad_selection
+    file ${WEPLAY_ROM} > /var/tmp/game-data.filetype
     if ( $(grep -qi html /var/tmp/game-data.filetype) );then
         bad_selection
     fi
-    export WEPLAY_ROM="/usr/src/game-data"
 fi
 
 if [[ -n "${REDIS_PORT}" ]];then
